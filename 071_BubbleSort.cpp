52fe09b300990a4f7636d4f8a45c5558c6eb72fc
Dhnnz12 <dhonanhibrizi123@gmail.com>
2024-03-08 15:13:07 +0700

    Deklarasi Varibael

--- 071_BubbleSort.cpp
+++ 071_BubbleSort.cpp
@@ -2,16 +2,7 @@
 #include <string>
 using namespace std;
 
-int a[20];
-int n;
+int a[20]; //Deklarasi variabel global array a dengan ukuran 20
+int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen array
 
-void input() {
-	while (true) {
-		cout << "Masukan banyaknya elemen pada array = ";
-		cin >> n;
-		if (n <= 20)
-			break;
-		else {
-			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
-		}
-	}
+int main()
